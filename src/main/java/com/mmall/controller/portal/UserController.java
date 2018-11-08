d3bd0c23171457969d8a3cf21d7f3fdd2cf4776b
haloer111 <345249961@qq.com>
2018-11-08 21:09:13 +0800

    add CookieUtil

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -5,6 +5,7 @@ import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
 import com.mmall.util.JsonUtil;
 import com.mmall.util.RedisPoolUtil;
 import org.apache.commons.lang3.StringUtils;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -36,10 +39,16 @@ public class UserController {
      */
     @RequestMapping(value = "login.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session) {
+    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse
+            httpServletResponse, HttpServletRequest request) {
         ServerResponse<User> user = iUserService.login(username, password);
         if (user.isSuccess()) {
+            //F1D230CAD45FAA6286F7AF6F8756AFB0
+            //F1D230CAD45FAA6286F7AF6F8756AFB0
 //            session.setAttribute(Const.CURRENT_USER, user.getData());
+            CookieUtil.writeLoginToken(httpServletResponse, session.getId());
+            CookieUtil.readLoginToken(request);
+            CookieUtil.delLoginToken(httpServletResponse, request);
             RedisPoolUtil.setEx(session.getId(), JsonUtil.obj2String(user.getData()), Const.RedisCacheExtime
                     .REDIS_SESSION_EXTIME);
         }
---
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -0,0 +1,58 @@
+package com.mmall.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author gexiao
+ * @date 2018/11/8 16:08
+ */
+@Slf4j
+public class CookieUtil {
+    public static final String COOKIE_DOMAIN = ".happymmall.com";
+    public static final String COOKIE_NAME = "mmall_login_token";
+
+    public static void writeLoginToken(HttpServletResponse response, String token) {
+        Cookie ck = new Cookie(COOKIE_NAME, token);
+        ck.setDomain(COOKIE_DOMAIN);
+        ck.setPath("/");//代表设置在根目录
+        //如果maxAge不设置，cookie就不会写入硬盘，而是写在内存，只在当前页面有效
+        ck.setMaxAge(60 * 60 * 24 * 365);//-1:永久
+        log.info("write cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
+        response.addCookie(ck);
+    }
+
+    public static String readLoginToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie ck : cookies) {
+                log.info("read cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
+                if (StringUtils.equals(ck.getName(), COOKIE_NAME)) {
+                    log.info("return cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
+                    return ck.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void delLoginToken(HttpServletResponse response, HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie ck : cookies) {
+                if (StringUtils.equals(ck.getName(), COOKIE_NAME)) {
+                    ck.setDomain(COOKIE_DOMAIN);
+                    ck.setPath("/");
+                    ck.setMaxAge(0);//设置成0，代表删除cookie
+                    log.info("del cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
+                    response.addCookie(ck);
+                    return;
+                }
+            }
+        }
+    }
+}
